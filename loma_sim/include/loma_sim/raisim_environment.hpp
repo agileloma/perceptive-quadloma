3b2bac09f344e1d56d46bc83d5be097df6ef792b
lijun-hitwh <junli@hit.edu.cn>
2025-06-27 12:19:53 +0800

    loma_sim init

---
+++ loma_sim/include/loma_sim/raisim_environment.hpp
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (c) 2025, Harbin Institute of Technology.
+ * All Rights Reserved
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file   raisim_environment.hpp
+ * @author Jun Li (junli@hit.edu.cn)
+ * @brief  Header file for RaisimEnvironment class
+ * @date   June 27, 2025
+ **/
+
+#pragma once
+
+
+namespace loma_sim {
+
+class RaisimEnvironment
+{
+public:
+    RaisimEnvironment();
+
+};
+
+}  // namespace loma_sim
---
+++ loma_sim/src/raisim_environment.cpp
@@ -0,0 +1,21 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (c) 2025, Harbin Institute of Technology.
+ * All Rights Reserved
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file   raisim_environment.cpp
+ * @author Jun Li (junli@hit.edu.cn)
+ * @brief  Source file for RaisimEnvironment class
+ * @date   June 27, 2025
+ **/
+
+#include "loma_sim/raisim_environment.hpp"
+
+
+namespace loma_sim {
+
+RaisimEnvironment::RaisimEnvironment() {}
+
+}  // namespace loma_sim
